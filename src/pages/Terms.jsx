102f02a88586f25f2ad6c1e855f30ad44e14de7c
Yashika Varshney <vyashika715@gmail.com>
2026-03-14 21:34:30 +0530

    debug

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from '../components/container/Container';
+import Container from '../components/container/container';
 import { Link } from 'react-router-dom';
 
 export default function Terms() {
